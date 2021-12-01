2c076f765695140f5e7158dcca993c886226f0b7
JGBe <94081823+JGBe@users.noreply.github.com>
2021-11-30 20:23:34 -0500

    Add files via upload
    Estas dos clases son el componente gráfico para la raqueta que es el ladrillo donde rebota la bolita en el cual se delimita que si la bolita pasa de la altura en la que este se encuentra el jugador pierde una vida y se inicia nuevamente

---
+++ Ladrillo.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class Ladrillo extends Objeto
+{
+   public int ancho;
+   public int largo;
+
+   public Ladrillo()
+      {
+	     super();
+         this.ancho = 48;
+         this.largo = 15;
+   }
+}
---
+++ Objeto.java
@@ -0,0 +1,14 @@
+package clases;
+
+import java.awt.Color;
+
+public class Objeto 
+   {
+	 public Color color;
+     public int xposicion;
+     public int yposicion;
+     
+     public Objeto()
+        {
+   	    }
+   }
