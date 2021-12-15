b7d6a04a3743b0e3b4aa3695e1678506eb1e263e
jagomezb1 <95323908+jagomezb1@users.noreply.github.com>
2021-12-14 22:02:07 -0500

    Update Main.java
    Versión final de la clase.

--- Main.java
+++ Main.java
@@ -3,8 +3,21 @@ import ladrillos.ladrillos;
 class Main
    {
 	  public static void main (String [] Args)
-         {
+	     {
             ladrillos juego = new ladrillos();
-            juego.run();
-         }
+            juego.setVisible(true);
+            while (true)
+               {
+                  juego.repaint();
+            	  juego.movimiento();
+            	  try 
+            	     {
+            		    Thread.sleep(30);
+            		 } 
+            	  catch (InterruptedException e) 
+            	     {
+            		    e.printStackTrace();
+            		 }
+               }
+		}
    }
