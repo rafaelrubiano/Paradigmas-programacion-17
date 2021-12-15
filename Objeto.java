b46a5514ff4efc989cc3e853ba62f6f80421a40f
JGBe <94081823+JGBe@users.noreply.github.com>
2021-12-14 21:52:54 -0500

    Add files via upload
    La clase objeto es la clase que hereda a bolita y ladrillo  y la clase miscolores es la que tiene los datos de los colores para los ladrillos

---
+++ Objeto.java
@@ -0,0 +1,16 @@
+package clases;
+
+import java.awt.Color;
+
+public class Objeto 
+   {
+	 public Color color;
+     public int xposicion;
+     public int yposicion;
+     public boolean visible;
+     
+     public Objeto()
+        {
+    	 visible = true;
+   	    }
+   }
